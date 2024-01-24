8bcfa5869953eecabff8f1ea095a0775b45ccd82
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-01-24 17:42:05 +0300

    first commit

---
+++ src/test/java/com/flight/FlightSearchAPI/FlightSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.FlightSearchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
