0b3cc035459dd7f096c53a4905dbd803ab687c5e
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-01-25 02:55:23 +0300

    Entities completed!

---
+++ src/main/java/com/flight/FlightSearchAPI/entities/Token.java
@@ -0,0 +1,26 @@
+package com.flight.FlightSearchAPI.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token", schema = "flight")
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "expiry_date")
+    private LocalDateTime expiryDate;
+}
